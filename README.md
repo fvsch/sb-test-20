f5c82c03bb54de0caa6afa0c51b4c1dd0577cc4f
fvsch <florens@fvsch.com>
2022-08-30 16:58:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dependencies-filtering
+
+[Edit on StackBlitz ⚡️](https://local.stackblitz.com:3000/edit/dependencies-filtering)
---
+++ index.ts
@@ -0,0 +1,8 @@
+import 'remarkdown.css/dist/remarkdown.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `
+  <h1>TypeScript Starter</h1>
+  <p>Is this thing on? One two, one two.</p>
+`;
